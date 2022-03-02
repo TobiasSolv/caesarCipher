339c8dfe5d5baa21ceb9d400ebe30c46baa72d10
Tobias <tobiasurban@hotmail.com>
2022-03-02 11:50:20 +0100

    made deencrypt

--- src/caesarCipher.java
+++ src/caesarCipher.java
@@ -14,7 +14,9 @@ public class caesarCipher {
     boolean yes = true;
     String alfabet = " ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
 
+
     String userinput1;
+
 
 
     void intro() {
@@ -25,26 +27,33 @@ public class caesarCipher {
     void encrypt() {
 
         if (userinput1.equals("1")) {
+            int numbers = 0;
             System.out.print("Write text you want encrypted: ");
             userinput1 = in.nextLine().toUpperCase(Locale.ROOT);
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < 10; i++) {
                 char chinput = in.next().charAt(0);
-                chinput = Character.toUpperCase(chinput);
                 int indexOfChar = alfabet.indexOf(chinput);
-                System.out.println(indexOfChar);
+                numbers[i] = indexOfChar;
+                System.out.println(numbers);
+
 
             }
             // bliver encrypted
         }
     }
 
-    void decrypt(){
-            if (userinput1.equals("2")) {
-                System.out.println("Write text you want decrypted: ");
-                userinput1 = in.nextLine().toUpperCase();
+    void decrypt() {
+        if (userinput1.equals("2")) {
+            System.out.println("Write text you want decrypted: ");
+            userinput1 = in.nextLine().toUpperCase();
+            for (int i = 0; i < 5; i++) {
+                int intinput = in.nextInt();
+                char inputLetter = alfabet(intinput);
+                System.out.println(inputLetter);
                 // bliver decrypted
             }
         }
+    }
 
         void exit () {
         if (userinput1.equals("exit")){
