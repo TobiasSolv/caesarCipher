262fb1e869b91d904077a2b64d8fae712c45f9a0
Tobias <tobiasurban@hotmail.com>
2022-03-02 09:49:52 +0100

    Start

---
+++ src/caesarCipher.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class caesarCipher {
+
+    void go(){
+        intro();
+
+    }
+
+
+    void intro(){
+        Scanner in = new Scanner(System.in);
+
+        String userinput1;
+
+        System.out.print("Encrypt type 1 or Decrypt type 2: ");
+        userinput1 = in.nextLine();
+
+        if (userinput1.equals("1")) {
+            System.out.print("Write text you want encrypted: ");
+            userinput1 = in.nextLine();
+            // bliver encrypted
+        }
+
+        else if (userinput1.equals("2")){
+            System.out.println("Write text you want decrypted");
+            userinput1 = in.nextLine();
+            // bliver decrypted
+
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+        new caesarCipher().go();
+    }
+
+}
