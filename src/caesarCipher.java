a2a78b68431628bb03d6a3738eb2519cf584bd73
Tobias <tobiasurban@hotmail.com>
2022-03-02 11:07:53 +0100

    made methods

--- src/caesarCipher.java
+++ src/caesarCipher.java
@@ -5,42 +5,66 @@ public class caesarCipher {
 
     void go() {
         intro();
-
+        encrypt();
+        decrypt();
+        exit();
     }
 
+    Scanner in = new Scanner(System.in);
+    boolean yes = true;
+    String alfabet = " ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+
+    String userinput1;
+    char userinput2;
 
     void intro() {
-        Scanner in = new Scanner(System.in);
-        boolean yes = true;
-        String alfabet = " ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
-
-        String userinput1;
-
-        System.out.print("Encrypt type 1 or Decrypt type 2: ");
+        System.out.print("Encrypt type 1, Decrypt type 2 or exit to exit: ");
         userinput1 = in.nextLine();
+    }
+
+    void encrypt() {
 
         if (userinput1.equals("1")) {
             System.out.print("Write text you want encrypted: ");
             userinput1 = in.nextLine().toUpperCase(Locale.ROOT);
+            for (int i = 0; i < alfabet.length(); i++) {
+                char chinput = in.next().charAt(0);
+                int indexOfChar = chinput;
+
+            }
             // bliver encrypted
+        }
+    }
 
-        } else if (userinput1.equals("2")) {
-            System.out.println("Write text you want decrypted: ");
-            userinput1 = in.nextLine().toUpperCase();
-            // bliver decrypted
+    void decrypt(){
+            if (userinput1.equals("2")) {
+                System.out.println("Write text you want decrypted: ");
+                userinput1 = in.nextLine().toUpperCase();
+                // bliver decrypted
+            }
+        }
 
-        } else {
+        void exit () {
             userinput1.equals("exit");
             yes = false;
             //exit
         }
 
 
-    }
 
 
-    public static void main(String[] args) {
+    public static void main (String[]args){
         new caesarCipher().go();
     }
 
-}
+    }
+
+
+
+
+
+
+
+
+
+
