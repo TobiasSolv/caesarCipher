f9b77299af10efc9ff78879dbcbe2a01eaee24dc
Tobias <tobiasurban@hotmail.com>
2022-03-02 10:31:31 +0100

    Made exit

--- src/caesarCipher.java
+++ src/caesarCipher.java
@@ -1,15 +1,18 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class caesarCipher {
 
-    void go(){
+    void go() {
         intro();
 
     }
 
 
-    void intro(){
+    void intro() {
         Scanner in = new Scanner(System.in);
+        boolean yes = true;
+        String alfabet = " ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
 
         String userinput1;
 
@@ -18,15 +21,18 @@ public class caesarCipher {
 
         if (userinput1.equals("1")) {
             System.out.print("Write text you want encrypted: ");
-            userinput1 = in.nextLine();
+            userinput1 = in.nextLine().toUpperCase(Locale.ROOT);
             // bliver encrypted
-        }
 
-        else if (userinput1.equals("2")){
-            System.out.println("Write text you want decrypted");
-            userinput1 = in.nextLine();
+        } else if (userinput1.equals("2")) {
+            System.out.println("Write text you want decrypted: ");
+            userinput1 = in.nextLine().toUpperCase();
             // bliver decrypted
 
+        } else {
+            userinput1.equals("exit");
+            yes = false;
+            //exit
         }
 
 
